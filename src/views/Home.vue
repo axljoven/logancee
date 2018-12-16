23f801ed149e037ca1d7b417021a04fb46e273dc
Axl Joven <axlmorenojoven@gmail.com>
2018-12-16 23:53:15 +0800

    Added header, footer and banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// Import Banner
+import Banner from '@/components/Banner.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Banner
   },
 };
 </script>
